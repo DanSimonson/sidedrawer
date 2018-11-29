cffd1111878670dce124dc3444bf2a25d9356396
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2018-11-30 02:28:06 +0800

    started a sidedrawer

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,58 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class='test' v-on:click="active = !active" v-bind:class={active:active}>
+      <svg width="30" height="30">
+        <path d="M0,5 30,5" stroke="#111" stroke-width="5" />
+        <path d="M0,14 30,14" stroke="#111" stroke-width="5" />
+        <path d="M0,23 30,23" stroke="#111" stroke-width="5" />
+      </svg>
     </div>
-    <router-view/>
+
+    <!--<router-view/> -->
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+  export default {
+    data() {
+      return {
+        active: false
+      }
+    }
+  }
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style scoped>
+  .test {
+    border: 5px solid red;
+    width: 0;
+    height: 95vh;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  }
+
+  .active {
+    background-color: green;
+    width: 250px;
+  }
+
+  #app {
+    /*font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;*/
+  }
+
+  /*#nav {
+    padding: 30px;
+  }
+
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }*/
 </style>

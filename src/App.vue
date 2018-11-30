170e96c9f7ed91ca89568fdf5a28c51daca1e319
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2018-11-30 21:47:02 +0800

    first draft of sidedrawer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <div class='test' v-on:click="active = !active" v-bind:class={active:active}>
-      <svg class='burger' width="30" height="30">
-        <path d="M0,5 30,5" stroke="#111" stroke-width="5" />
-        <path d="M0,14 30,14" stroke="#111" stroke-width="5" />
-        <path d="M0,23 30,23" stroke="#111" stroke-width="5" />
-      </svg>
+    <div class='nav-bar' @click="active = !active" v-bind:class={active:active}>
+      <transition name="fade">
+        <svg v-if="!active" class='burger' width="30" height="30">
+          <path d="M0,5 30,5" stroke="#111" stroke-width="5" />
+          <path d="M0,14 30,14" stroke="#111" stroke-width="5" />
+          <path d="M0,23 30,23" stroke="#111" stroke-width="5" />
+        </svg>
+      </transition>
+      <a href="#" class="btn-close"><i class="fas fa-times"></i></a>
+      <div id="side-menu" class="side-nav">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+      </div>
     </div>
-
     <!--<router-view/> -->
   </div>
 </template>
@@ -23,6 +31,10 @@
 </script>
 
 <style scoped>
+  .burger:hover {
+    cursor: pointer;
+  }
+
   .burger {
     top: 20px;
     right: 25px;
@@ -33,40 +45,69 @@
     position: relative;
   }
 
-  .test {
+  .nav-bar {
+    /*border: 5px solid red;*/
     position: absolute;
-    border: 5px solid red;
     width: 0;
     height: 95vh;
     top: 0;
     right: 0;
+    position: fixed;
+    z-index: 1;
+    opacity: 0.9;
+    overflow-x: hidden;
+    padding-top: 60px;
+    background-color: #BBDEFB;
+    transition: .5s;
 
   }
 
-  .active {
-    background-color: green;
-    /*width: 250px;*/
+  .nav-bar.active {
+    background-color: #BBDEFB;
     width: calc(2px + 30%);
   }
 
-  #app {
-    /*font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;*/
+  .side-nav {
+    /*border: 5px solid black;*/
+    display: flex;
+    height: 40%;
+    margin-top: 50px;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
   }
 
-  /*#nav {
-    padding: 30px;
+  .side-nav a {
+    text-decoration: none;
   }
 
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
+  .side-nav a:hover {
+    color: #fff;
+    text-decoration: none;
   }
 
-  #nav a.router-link-exact-active {
-    color: #42b983;
-  }*/
+  .nav-bar .btn-close {
+    position: absolute;
+    top: 0;
+    left: 22px;
+    font-size: 36px;
+    /*
+    right: 22px;
+    margin-left: 50px;
+    */
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity .5s;
+    transition-delay: .5s;
+  }
+
+  .fade-enter,
+  .fade-leave-to
+
+  /* .fade-leave-active in <2.1.8 */
+    {
+    opacity: 0;
+  }
 </style>
